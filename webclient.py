d8deed496e203a5743e42ae8ae830275c6032971
MarCastellnou <marccastellnou98@gmail.com>
2019-02-19 11:47:17 +0100

    prints activities list

--- webclient.py
+++ webclient.py
@@ -1,5 +1,6 @@
 
 from urllib.request import urlopen
+import bs4
 
 class webClient(object):
     """webClient class"""
@@ -13,12 +14,23 @@ class webClient(object):
         f.close()
         return page
 
+    def search_activities(self, page):
+        tree = bs4.BeautifulSoup(page,"lxml")
+        activities = tree.find_all("div","featured-links-item")
+        act_list=[]
+        for activity in activities:
+            title = activity.find("spam","flink-title")
+            link=activity.find("a")
+            act_list.append((title.text,link["href"]))
+            return act_list
+
     def run(self):
         #download a web page
         page = self.download_page()
         #search activity in a web page
+        data = self.search_activities(page)
         #print the activities
-        print(page)
+
 
 
 if __name__ == "__main__":
