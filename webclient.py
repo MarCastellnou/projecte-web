01fead7d3a21d01542350d1391d0d6e51d1cfa28
MarCastellnou <marccastellnou98@gmail.com>
2019-02-26 11:23:24 +0100

    xmltodict

--- webclient.py
+++ webclient.py
@@ -14,15 +14,22 @@ class webClient(object):
         f.close()
         return page
 
+    #def search_activities(self, page):
+    #    tree = bs4.BeautifulSoup(page,"lxml")
+    #    activities = tree.find_all("div","featured-links-item")
+    #    act_list=[]
+    #    for activity in activities:
+    #        title = activity.find("span","flink-title")
+    #        link = activity.find("a")
+    #        act_list.append((title.text,link["href"]))
+    #        return act_list
+
     def search_activities(self, page):
-        tree = bs4.BeautifulSoup(page,"lxml")
-        activities = tree.find_all("div","featured-links-item")
-        act_list=[]
-        for activity in activities:
-            title = activity.find("span","flink-title")
-            link = activity.find("a")
-            act_list.append((title.text,link["href"]))
-            return act_list
+
+        xml = xmltodict.parse(page)
+        pprint.pprint(xml)
+        return None
+
 
     def run(self):
         #download a web page
