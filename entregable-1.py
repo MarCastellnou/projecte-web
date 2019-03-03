5db44c156ac46aaf4c875e8b43238de36bdc526a
MarCastellnou <marccastellnou98@gmail.com>
2019-03-03 14:26:24 +0100

    Finalizado

--- entregable-1.py
+++ entregable-1.py
@@ -7,27 +7,42 @@ class Pagina(object):
         super(Pagina, self).__init__()
 
     def download_page(arg):
-
         f = urlopen("https://www.banggood.com/Flashdeals.html")
         page = f.read()
         f.close()
         return page
 
-    def search_offer(self,page):
+    def page_lxml(self,page):
         tree = bs4.BeautifulSoup(page,"lxml")
-        text = tree.find_all("li", data_product_id='')
-        list=[]
+        return tree
+
+    def search_prices(self,page):
+        text = page.find_all("div","priceitem")
+        list_price=[]
         for activity in text:
             offer_p = activity.find("span","price")
             original_p = activity.find("span","price_old")
-            name = activity.find("span","title")
-            list.append((name.text,offer_p.text, original_p.text))
-        return list
+            list_price.append((offer_p.text, original_p.text))
+        return list_price
+
+    def search_name_product(self,page):
+        text1 = page.find_all("span","title")
+        list_name=[]
+        for activity in text1:
+            name = activity.find("a")
+            list_name.append(name.text)
+        return list_name
+
+    def print_data(self,price,name):
+        for x in range(len(price)):
+            print("\nNombre del producto= ",name[x],"\nPrecio en oferta= ",price[x][0],"\nPrecio original= ",price[x][1])
 
     def run(self):
         page = self.download_page()
-        data = self.search_offer(page)
-        print(data)
+        lxml = self.page_lxml(page)
+        price = self.search_prices(lxml)
+        name = self.search_name_product(lxml)
+        self.print_data(price,name)
 
 
 if __name__=="__main__":
